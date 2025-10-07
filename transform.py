2ddb64fb21bbfaf7ab777945aced0561da17285b
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2025-10-06 22:26:28 -0300

    removendo transform

--- transform.py
+++
@@ -1,9 +0,0 @@
-DATA_FILE = "250923"
-FILE_NAME = "BGBG186_{DATA_FILE}.xml"
-
-def transform ():
-    xml_storage_file=get_file_from_blob(FILE_NAME)
-    print(xml_storage_file)  
-
-transform() 
-    
